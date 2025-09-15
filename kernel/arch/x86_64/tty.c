08ff60e254199ccf6c15ed690830bd92e2c841d7
Alanna Tempest <alannayt@gmail.com>
2025-09-15 12:56:10 -0400

    make prints go to terminal via qemu

--- kernel/arch/x86_64/tty.c
+++ kernel/arch/x86_64/tty.c
@@ -5,7 +5,42 @@
 
 #include <kernel/tty.h>
 
-#include "vga.h"
+// #include "vga.h"
+#include <stdint.h>
+
+static inline void outb(uint16_t port, uint8_t val) {
+	__asm__ volatile ("outb %0, %1" : : "a"(val), "Nd"(port));
+}
+
+static inline uint8_t inb(uint16_t port) {
+	uint8_t ret;
+	__asm__ volatile ("inb %1, %0" : "=a"(ret) : "Nd"(port));
+	return ret;
+}
+
+static int serial_initialized = 0;
+
+static void serial_init(void) {
+	if (serial_initialized) return;
+	/* COM1 base 0x3F8 */
+	outb(0x3F8 + 1, 0x00);    // Disable all interrupts
+	outb(0x3F8 + 3, 0x80);    // Enable DLAB
+	outb(0x3F8 + 0, 0x01);    // Set divisor to 1 (115200 baud)
+	outb(0x3F8 + 1, 0x00);
+	outb(0x3F8 + 3, 0x03);    // 8 bits, no parity, one stop bit
+	outb(0x3F8 + 2, 0xC7);    // Enable FIFO, clear them, with 14-byte threshold
+	outb(0x3F8 + 4, 0x0B);    // IRQs enabled, RTS/DSR set
+	serial_initialized = 1;
+}
+
+static int serial_is_transmit_empty() {
+	return inb(0x3F8 + 5) & 0x20;
+}
+
+static void serial_write_char(char a) {
+	while (serial_is_transmit_empty() == 0) {}
+	outb(0x3F8, (uint8_t)a);
+}
 
 static const size_t VGA_WIDTH = 80;
 static const size_t VGA_HEIGHT = 25;
@@ -17,16 +52,7 @@ static uint8_t terminal_color;
 static uint16_t* terminal_buffer;
 
 void terminal_initialize(void) {
-	terminal_row = 0;
-	terminal_column = 0;
-	terminal_color = vga_entry_color(VGA_COLOR_LIGHT_GREY, VGA_COLOR_BLACK);
-	terminal_buffer = VGA_MEMORY;
-	for (size_t y = 0; y < VGA_HEIGHT; y++) {
-		for (size_t x = 0; x < VGA_WIDTH; x++) {
-			const size_t index = y * VGA_WIDTH + x;
-			terminal_buffer[index] = vga_entry(' ', terminal_color);
-		}
-	}
+	serial_init();
 }
 
 void terminal_setcolor(uint8_t color) {
@@ -34,8 +60,8 @@ void terminal_setcolor(uint8_t color) {
 }
 
 void terminal_putentryat(unsigned char c, uint8_t color, size_t x, size_t y) {
-	const size_t index = y * VGA_WIDTH + x;
-	terminal_buffer[index] = vga_entry(c, color);
+	(void)color; (void)x; (void)y;
+	serial_write_char((char)c);
 }
 
 static void terminal_advance_row() {
