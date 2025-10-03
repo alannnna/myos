144610f42a82f8d074a588cf016627385cd7f469
Alanna <alannayt@gmail.com>
2025-10-03 09:38:04 -0400

    move inb/outb into header for sharing

--- kernel/arch/x86_64/tty.c
+++ kernel/arch/x86_64/tty.c
@@ -4,19 +4,10 @@
 #include <string.h>
 
 #include <kernel/tty.h>
+#include <arch/x86_64/asm.h>
 
 // #include "vga.h"
 #include <stdint.h>
-
-static inline void outb(uint16_t port, uint8_t val) {
-	__asm__ volatile ("outb %0, %1" : : "a"(val), "Nd"(port));
-}
-
-static inline uint8_t inb(uint16_t port) {
-	uint8_t ret;
-	__asm__ volatile ("inb %1, %0" : "=a"(ret) : "Nd"(port));
-	return ret;
-}
 
 static int serial_initialized = 0;
 
---
+++ kernel/include/arch/x86_64/asm.h
@@ -0,0 +1,16 @@
+#ifndef ARCH_X86_64_ASM_H
+#define ARCH_X86_64_ASM_H
+
+#include <stdint.h>
+
+inline void outb(uint16_t port, uint8_t val) {
+	__asm__ volatile ("outb %0, %1" : : "a"(val), "Nd"(port));
+}
+
+inline uint8_t inb(uint16_t port) {
+	uint8_t ret;
+	__asm__ volatile ("inb %1, %0" : "=a"(ret) : "Nd"(port));
+	return ret;
+}
+
+#endif
